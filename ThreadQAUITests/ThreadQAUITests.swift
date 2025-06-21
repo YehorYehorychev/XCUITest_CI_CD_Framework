518d94d669caae77ecc61bd469fe176c2e41140d
Yehor Yehorychev <egor57899@gmail.com>
2025-06-20 17:31:21 -0700

    Fist dummy UI test has been created for setup testing purposes

--- ThreadQAUITests/ThreadQAUITests.swift
+++ ThreadQAUITests/ThreadQAUITests.swift
@@ -9,43 +9,18 @@ import XCTest
 
 final class ThreadQAUITests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
+    // Simple/dummy UI test example
     @MainActor
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    func testSumOfTwoNumbers() {
         let app = XCUIApplication()
         app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        app.buttons["loginBtn"].tap()
+        
+        let expectedErrorText = "Email and password should not be empty"
+        let errorMessageElement = app.staticTexts[expectedErrorText]
+        
+        XCTAssertTrue(errorMessageElement.exists, "Error message is not shown")
+        XCTAssertEqual(errorMessageElement.label, expectedErrorText)
     }
-
-    @MainActor
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
-    
-    // Simple/dummy test example
-    func sumOfTwoNumbers() {
-        var a = 5
-        var b = 10
-        var result = a + b
-        XCTAssertEqual(result, 15)
-    }git init
 }
